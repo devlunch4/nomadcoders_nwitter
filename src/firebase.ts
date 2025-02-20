e17e9741b8d38092bdfddd4a5b33ac57aee8721a
devlunch4 <devlunch4@gmail.com>
2025-02-20 19:29:19 +0000

    #2.3 Firebase Project

---
+++ src/firebase.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDIr7p4mzLDdFet5zaFvwH-Z3DaPSEzGG8",
+  authDomain: "nwitter-v2.firebaseapp.com",
+  projectId: "nwitter-v2",
+  storageBucket: "nwitter-v2.firebasestorage.app",
+  messagingSenderId: "714026600125",
+  appId: "1:714026600125:web:b01336fb502ca161db19d4",
+  measurementId: "G-8XRMP88MXS"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
