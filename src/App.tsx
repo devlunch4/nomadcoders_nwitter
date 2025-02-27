451d283a0910124aefa9326c0c9b469c9ba44a8d
devlunch4 <devlunch4@gmail.com>
2025-02-27 11:33:26 +0000

    #3.1 Forms and UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,40 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom"
-import Layout from "./components/layout"
-import Home from "./routes/home"
-import Profile from "./routes/profile"
-import Login from "./routes/login"
-import CreateAccount from "./routes/create-account"
-import { createGlobalStyle } from "styled-components"
-import reset from "styled-reset"
-import { useEffect, useState } from "react"
-import LoadingScreen from "./components/loading-screen"
-import { auth } from "./firebase"
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Layout from "./components/layout";
+import Home from "./routes/home";
+import Profile from "./routes/profile";
+import Login from "./routes/login";
+import CreateAccount from "./routes/create-account";
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+import { useEffect, useState } from "react";
+import LoadingScreen from "./components/loading-screen";
+import { auth } from "./firebase";
+import { styled } from "styled-components";
 
-const router = createBrowserRouter(
-  [
-    {
-      path: "/",
-      element: <Layout />,
-      children: [
-        {
-          path: "",
-          element: <Home />,
-        },
-        {
-          path: "profile",
-          element: <Profile />,
-        },
-      ],
-    },
-    {
-      path: "/login",
-      element: <Login />,
-    },
-    {
-      path: "/create-account",
-      element: <CreateAccount />,
-    },
-  ]
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "profile",
+        element: <Profile />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/create-account",
+    element: <CreateAccount />,
+  },
+]);
 
 const GlobalStyles = createGlobalStyle`
   ${reset};
@@ -53,6 +52,11 @@ const GlobalStyles = createGlobalStyle`
   }
   `;
 
+const Wrapper = styled.div`
+  height: 100%;
+  display: flex;
+  justify-content: center;
+`;
 function App() {
   const [isLoading, setLoading] = useState(true);
   const init = async () => {
@@ -60,16 +64,14 @@ function App() {
     setLoading(false);
   };
   useEffect(() => {
-    init()
+    init();
   }, []);
   return (
-    <>
+    <Wrapper>
       <GlobalStyles />
-      {
-        isLoading ? <LoadingScreen /> : <RouterProvider router={router} />
-      }
-    </>
-  )
+      {isLoading ? <LoadingScreen /> : <RouterProvider router={router} />}
+    </Wrapper>
+  );
 }
 
 export default App;
